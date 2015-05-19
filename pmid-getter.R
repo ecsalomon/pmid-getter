1d411a66dbd8181d8978cbf669663716cefb84e5
Erika Salomon <ecsalomon@gmail.com>
2015-05-19 18:08:16 -0500

    pmid getter
    A function to get pubmed IDs for a given journal in a given year

---
+++ pmid-getter.R
@@ -0,0 +1,48 @@
+# Date: 2015-05-19
+# Author: Erika Salomon
+# Email: ecsalomon@gmail.com
+# Purpose: Returns a vector of PubMed IDs for a given journal in a given year
+# Packages used: XML, rCurl
+# Licence: CC BY-SA-NC
+
+# Parameters:
+# journal : A string of the exact journal title
+# year : The publication year desired
+# maxResults : The maximum number of results to return; defaults to 1000
+# browser : the browser to use for obtaining PMIDs; options are "firefox", "chrome", "safari",
+#           "internet explorer", and "opera"; defaults to "firefox"
+
+pullPMIDs <- function(journal, year, maxResults=1000, browser="firefox") {
+  ### Load packages ###
+  require(XML)
+  require(RSelenium)
+  
+  ### Start Selenium browser ###
+  capture.output(RSelenium::checkForServer())
+  capture.output(RSelenium::startServer())
+  capture.output(remDr <- RSelenium::remoteDriver(browserName = browser))
+  capture.output(remDr$open())
+  
+  ### Build the URI ###
+  uri <- paste('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22', 
+               journal, '%22[Journal]+AND+', year, '[pdat]&retmax=', maxResults, sep="")
+  
+  ### Get PubMed IDs ### 
+  remDr$navigate(uri)
+  pageSource <- remDr$getPageSource(header = TRUE)
+  pageXML <- xmlTreeParse(pageSource, asText=TRUE)
+  pageRoot <- xmlRoot(pageXML)  
+  # count <- as.integer(xmlValue(pageRoot[[1]]))
+  idList <- xmlChildren(pageRoot[[4]])
+  ids <- as.integer(sapply(idList, xmlValue))
+  
+  ### Close Selenium Browser ###
+  remDr$close()
+  
+  ### Return List of IDs ###
+  return(ids)
+}
+
+
+  
+  
